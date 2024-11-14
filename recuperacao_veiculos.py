6f32c776f80c4281a3d47b0a84abc7c48e908ef0
B-Fonsec4 <mottabrenno@gmail.com>
2024-11-14 20:04:42 -0300

    codigo finalizado

--- recuperacao_veiculos.py
+++ recuperacao_veiculos.py
@@ -14,3 +14,57 @@ try:
 except ImportError as e:
     print(f'Erro: {e}')
     exit()
+
+
+try:
+    array_recupera_veiculo = np.array(df_recuperacao_veiculo['recuperacao_veiculos'])
+    media_recupera = np.mean(array_recupera_veiculo)
+    mediana_recupera = np.median(array_recupera_veiculo)
+    distancia = abs((media_recupera - mediana_recupera)/ mediana_recupera) * 100
+
+    maximo = np.max(array_recupera_veiculo)
+    minimo = np.min(array_recupera_veiculo)
+    amplitude_total = maximo - minimo
+
+    q1 = np.quantile(array_recupera_veiculo, 0.25, method='weibull')
+    q2 = np.quantile(array_recupera_veiculo, 0.50, method='weibull')
+    q3 = np.quantile(array_recupera_veiculo, 0.75, method='weibull')
+    iqr = q3 - q1
+    limite_superior = q3 +(1.5 * iqr)
+    limite_inferior = q1 - (1.5 * iqr)
+    print(30* '-')
+    print("Mínimo:", minimo)
+    print(f'Limite inferior: { limite_inferior}')
+    print(f'Q1: {q1}')
+    # print(f'Q2: {q2}')
+    print(f'Q3: {q3}')
+    print('IQR: ',iqr)
+    print(f'Limite superior: {limite_superior}')
+    print('Máximo:', maximo)
+    print(f'Amplitude total:{amplitude_total:.2f}')
+
+    print( 'Resultados!!!')
+    print(f'Essa é a media: {media_recupera:.2f}')
+    print(f'Essa é a mediana: {mediana_recupera:.2f}')
+    print(f'Essa é a distancia: {distancia:.2f}')
+
+    print(30*'-')
+    df_outiliers_sup = df_recuperacao_veiculo[df_recuperacao_veiculo['recuperacao_veiculos'] > limite_superior]
+    print('\n DPs com recuperações superiores as demais: ')
+    print(30*'-')
+    if len(df_outiliers_sup)==0:
+        print('Não existe DPs com valores discrepantes superiores')
+    else:
+        print(df_outiliers_sup.sort_values(by='recuperacao_veiculos', ascending=False))
+    
+    df_outiliers_inf = df_recuperacao_veiculo[df_recuperacao_veiculo['recuperacao_veiculos'] < limite_inferior]
+    print('\n DPs com recuperações inferiores as demais: ')
+    print(30*'-')
+    if len(df_outiliers_inf) == 0:
+        print('\n Não existe DPs com valores discrepantes inferiores')
+    else:
+        print(df_outiliers_inf.sort_values(by='recuperacao_veiculos', ascending=True))
+
+ 
+except ImportError as e:
+    print(f'Erro: {e}')
